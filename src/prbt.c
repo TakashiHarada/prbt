21f900ca9fee7cdeaf73f452cea870401bd9dddb
falke727 <r201470039hs@kanagawa-u.ac.jp>
2016-07-07 14:44:29 +0900

    make a frame

--- src/prbt.c
+++ src/prbt.c
@@ -2,13 +2,29 @@
 
 #include <prbt.h>
 
-void low_trie_traverse(prbt* low, prbt** PT)
+void low_trie_traverse(prbt* high, prbt** PT)
 {
-	unsigned l = strlen(low->label);
-	char* bit_string = (char*)malloc((l+1)*sizeof(char));
-	strcpy(bit_string, low->label);
-	bit_string[l] = '\0';
+	unsigned len = strlen(high->label);
+	char* bit_string = (char*)malloc((len+1)*sizeof(char));
+	strcpy(bit_string, high->label);
+	bit_string[len] = '\0';
 
+	{ unsigned i, l, j = 0;
+		prbt* low;
+		for (i = high->trie_number; i < _w-1; ++i) {
+			low = PT[i+1];
+			for (l = j; l < len; ++l) {
+				if ('0' == bit_string[l] && NULL != low->left) { low = low->left; }
+				else if ('1' == bit_string[l] && NULL != low->right) { low = low->right; }
+				else { break; }
+			}
+			if (l == len) {
+				if (NULL == high->left || NULL == high->right) { ; }
+				if (NULL != low->rs) { ; }
+				break;
+			}
+		}
+	}
 	//printf("%d | %c | %s\n", low->trie_number, low->bit, bit_string);
 
 	free(bit_string);
@@ -119,14 +135,14 @@ prbt** make_Pointed_Run_Based_Trie(char** rulelist)
 		prbt* pj;
 		/* if a root node of PT[i] does not have left of right or both childs, 
 		 * then each pointers of root of PT[i] points to root of PT[j] */
-		for (i = 0; i < _w-1; ++i) { // PT[w] needs not to have both pointers.
+		for (i = 0; i < _w-1; ++i) { // PT[w-1] needs not to have both pointers.
 			pi = PT[i];
 			pj = PT[i+1];
 			make_pointer_from_PTi_to_PTj(pi, pj);   // PT_(j) means PT_(i+1)
 		}
 
 		prbt* low_trie;
-		for (i = _w-1; ; --i) {
+		for (i = _w-2; ; --i) { // start from PT[w-2]
 			low_trie = PT[i];
 			lower_trie_traverse_via_label_of_runs_on_higher_trie(low_trie, PT);
 			if (i == 0) { break; }
