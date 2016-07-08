3a41ef60843b7bc2f2491b136100b8e2d3312d76
falke727 <r201470039hs@kanagawa-u.ac.jp>
2016-07-08 10:56:17 +0900

    measer a classification time

--- src/classify.c
+++ src/classify.c
@@ -26,21 +26,30 @@ void sequential_search(char** rulelist, char* header)
     defaultrule[j] = '*';
   defaultrule[_w] = '\0';
 
+	/*
   if (i != _n)
     //printf("%s -> rule[%2d] %s\n", header, i+1, rulelist[i]);
     printf("%s --> %2d\n", header, i+1);
   else
     //printf("%s -> rule[%2d] %s\n", header, _n+1, defaultrule);
     printf("%s --> %2d\n", header, i+1);
+	*/
 }
 
 void do_sequential_search(char** rulelist, char** headerlist)
 {
 	printf("=============== Sequential Search =============== \n");
+	struct timeval start_time, end_time;
+	double sec_time_of_day;
   unsigned i;
+	gettimeofday(&start_time, NULL);
   for (i = 0; i < _hn; ++i)
     sequential_search(rulelist, headerlist[i]);
+	gettimeofday(&end_time, NULL);
 	printf("================================================= \n");
+	sec_time_of_day = (end_time.tv_sec - start_time.tv_sec) 
+		+ (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
+	printf("%d -- %f\n", _hn, sec_time_of_day);
 }
 
 void simple_search(rbt** T, char* header)
@@ -72,7 +81,7 @@ void simple_search(rbt** T, char* header)
       }
     }
   }
-  printf("%s --> %2d\n", header, priority);
+  //printf("%s --> %2d\n", header, priority);
 
   free(A);
 }
@@ -80,9 +89,16 @@ void do_simple_search(rbt** T, char** headerlist)
 {
 	printf("================= Simple Search ================= \n");
   unsigned i;
+	struct timeval start_time, end_time;
+	double sec_time_of_day;
+	gettimeofday(&start_time, NULL);
   for (i = 0; i < _hn; ++i)
     simple_search(T, headerlist[i]);
+	gettimeofday(&end_time, NULL);
 	printf("================================================= \n");
+	sec_time_of_day = (end_time.tv_sec - start_time.tv_sec) 
+		+ (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
+	printf("%d -- %f\n", _hn, sec_time_of_day);
 }
 
 void pointer_search(prbt** PT, char* header)
@@ -111,16 +127,23 @@ void pointer_search(prbt** PT, char* header)
 					}
 		}
 	}
-  printf("%s --> %2d\n", header, priority);
+  //printf("%s --> %2d\n", header, priority);
 
   free(A);
 }
 
 void do_pointer_search(prbt** PT, char** headerlist)
 {
-	printf("============ Pointed Run-Based Search =========== \n");
+	struct timeval start_time, end_time;
+	double sec_time_of_day;
   unsigned i;
+	printf("============ Pointed Run-Based Search =========== \n");
+	gettimeofday(&start_time, NULL);
   for (i = 0; i < _hn; ++i)
     pointer_search(PT, headerlist[i]);
 	printf("================================================= \n");
+	gettimeofday(&end_time, NULL);
+	sec_time_of_day = (end_time.tv_sec - start_time.tv_sec) 
+		+ (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
+	printf("%d -- %f\n", _hn, sec_time_of_day);
 }
