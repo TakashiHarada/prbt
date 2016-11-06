6b159d5a04e9ad9dddbccc0c27cdc62ccb274ce4
falke727 <r201470039hs@kanagawa-u.ac.jp>
2016-11-06 23:31:58 +0900

    add a base part

--- src/column_switching.c
+++ src/column_switching.c
@@ -6,21 +6,56 @@
 // RR[1] = (R[1],R[2]), RR[2] = (R[3],R[4]), .., RR[n/2] = (R[n-1],R[n])
 char*** devide_rulelist_to_2rulelists(char** rulelist)
 {
-  /* copy rulest to 2 dimension array A[n][w] */
-  char** A = (char**)malloc(_n*sizeof(char*));
+  unsigned n = (_n+1)/2;
+  char*** RR = (char***)malloc(n*sizeof(char**));
+
   {
-    unsigned i;
-    for (i = 0; i < _n; ++i) {
-      A[i] = (char*)malloc((_w+1)*sizeof(char)); // +1 for '\0'
-      strcpy(A[i],rulelist[i]);
-      printf("%s\n", A[i]);
+    unsigned i, j, k;
+    if (_n%2==0) {
+      for (i = 0, k = 0; i < n; ++i) {
+	RR[i] = (char**)malloc(2*sizeof(char*));
+	for (j = 0; j < 2; ++j) {
+	  RR[i][j] = (char*)malloc((_w+1)*sizeof(char));
+	  strcpy(RR[i][j], rulelist[k]);
+	  ++k;
+	}
+      }
+    }
+    else {
+      for (i = 0, k = 0; i < n-1; ++i) {
+	RR[i] = (char**)malloc(2*sizeof(char*));
+	for (j = 0; j < 2; ++j) {
+	  RR[i][j] = (char*)malloc((_w+1)*sizeof(char));
+	  strcpy(RR[i][j], rulelist[k]);
+	  ++k;
+	}
+      }
+      RR[n-1] = (char**)malloc(sizeof(char*));
+      RR[n-1][0] = (char*)malloc((_w+1)*sizeof(char));
+      strcpy(RR[n-1][0], rulelist[k]);
     }
   }
-  return NULL;
+
+  return RR;
 }
 
 prbt*** make_column_switched_Pointed_Run_Based_Trie(char **rulelist)
 {
   char*** rulelists = devide_rulelist_to_2rulelists(rulelist);
+  {
+    unsigned i, j, n = (_n+1)/2;
+    printf("%d hoge\n", _n);
+    if (_n%2 == 0) {
+      for (i = 0; i < n; ++i)
+  	for (j = 0; j < 2; ++j)
+  	  printf("%s\n", rulelists[i][j]);
+    }
+    else {
+      for (i = 0; i < n-1; ++i)
+  	for (j = 0; j < 2; ++j)
+  	  printf("%s\n", rulelists[i][j]);
+      printf("%s\n", rulelists[n-1][0]);
+    }
+  }
   return NULL;
 }
